90601c39894ec36a49aaee787c607e9ea52ab06e
vidhipungaliya <98946698+vidhipungaliya@users.noreply.github.com>
2022-05-12 21:39:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Status = "";
+input_text = "";
+
+function setup(){
+    canvas = createCanvas(300,290);
+    canvas.position(650,255);
+    video = createCapture(VIDEO);
+    video.size(300,290);
+    video.hide();
+}
+
+function draw (){
+    image(video, 0 , 0 , 300 , 290);
+}
+
+function start(){
+    object_Detector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+
+function modelLoaded(){
+    console.log("Model_Loaded");
+    Status = true;
+}
